e7650ed37deeebea853a41c043d9e295ae5df815
Samuel Kraus <samuelkraus@Samuels-MBP.lan>
2023-11-13 22:27:33 -0500

    adding places view with mock data

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 
 app.use('/places' , require('./controllers/places'))
+
 app.get('/', (req, res) => {
     res.render('home')
 })
