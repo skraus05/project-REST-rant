6c575dd40723338b1cdbf7de785263ffee8f9831
Samuel Kraus <samuelkraus@Samuels-MBP.lan>
2023-11-16 00:33:58 -0500

    adding bootstrap grids to index page

--- controllers/places.js
+++ controllers/places.js
@@ -5,13 +5,13 @@ router.get('/', (req, res) => {
         city: 'Seattle',
         state: 'WA',
         cuisines: 'Thai, Pan-Asian',
-        pic: 'http://placekitten.com/250/250'
+        pic: '/css/images/h-thai-ml-pic.jpg'
     }, {
         name: 'Coding Cate Cafe',
         city: 'Phoenix',
         state: 'AZ',
         cuisines: 'Coffee, Bakery',
-        pic: 'http://placekitten.com/250/250'
+        pic: '/css/images/coding-cafe-pic.jpg'
     }]
     res.render('places/index', {places})
 })
--- views/error404.jsx
+++ views/error404.jsx
@@ -7,6 +7,12 @@ function error404() {
             <main>
                 <h1>404: PAGE NOT FOUND</h1>
                 <p>Oops, sorry, we can't find this page!</p>
+                <div>
+                    <img src="/css/images/not-found-cat-pic.jpg" alt="Cute Cat Pic"/>
+                    <div>
+                        Photo by <a href="AUTHOR_LINK">Borna Bevanda</a> on <a href="UNSPLASH_LINK"> Unsplash</a>
+                    </div>
+                </div>
             </main>
         </Def>
     )
