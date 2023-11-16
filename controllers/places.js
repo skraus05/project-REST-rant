6c2d4b6cbadde37afdf9fb8f8ed8fc2af1bf0e03
Samuel Kraus <samuelkraus@Samuels-MBP.lan>
2023-11-16 01:10:45 -0500

    Jazzed up form page

--- controllers/places.js
+++ controllers/places.js
@@ -18,4 +18,7 @@ router.get('/', (req, res) => {
 router.get('/new', (req, res) => {
     res.render('places/new')
 })
+router.post('/', (req, res) => {
+    res.send('POST/places')
+})
 module.exports = router
