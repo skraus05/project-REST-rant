1d42096181c76f44c2a453c530ddda6ba5764ec9
Samuel Kraus <samuelkraus@Samuels-MBP.lan>
2023-11-18 13:08:57 -0500

    navbar with style

--- views/home.jsx
+++ views/home.jsx
@@ -7,7 +7,7 @@ function home() {
             <main>
                 <h1>HOME</h1>
                 <div>
-                    <img src="/css/images/chia-fruit-drink.jpg" alt="Chia Fruit Shake"/>
+                    <img src="/css/images/chia-fruit-drink.jpg" alt="Chia Fruit Shake" className='img-fluid w-50 h-auto'/>
                     <div>
                         Photo by <a href="AUTHOR_LINK">Brenda Godinez</a> on<a href="UNSPLASH_LINK"> Unsplash</a>
                     </div>
