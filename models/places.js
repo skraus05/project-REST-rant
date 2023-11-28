8539b4224c17169fefc76641c7e2f7d3ef599a80
Samuel Kraus <samuelkraus@Samuels-MBP.lan>
2023-11-27 22:23:47 -0500

    used mongoose and updated stub links

--- models/places.js
+++ models/places.js
@@ -1,13 +1,10 @@
-    module.exports = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: '/css/images/h-thai-ml-pic.jpg'
-    }, {
-        name: 'Coding Cate Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: '/css/images/coding-cafe-pic.jpg'
-    }]
+ const mongoose = require('mongoose')
+ const placeSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    pic: String,
+    cuisines: { type: String, required: true },
+    city: { type: String, default: 'Anytown' },
+    state: { type: String, default: 'USA' },
+    founded: Number
+ })
+ module.exports = mongoose.model('Place', placeSchema)
