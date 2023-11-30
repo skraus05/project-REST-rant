0e3aa50c2cc693d277f5064fb6947be52ed4dd68
Samuel Kraus <samuelkraus@Samuels-MBP.lan>
2023-11-29 21:11:59 -0500

    adding node seeders command

---
+++ seeders/seed-places.js
@@ -0,0 +1,25 @@
+const db = require('../models')
+
+db.Place.create([{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: '/css/images/h-thai-ml-pic.jpg',
+    founded: 1989
+}, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: '/css/images/coding-cafe-pic.jpg',
+    founded: 2020
+}])
+.then(() => {
+    console.log('Success!')
+    process.exit()
+})
+.catch(err => {
+    console.log('Failure!', err)
+    process.exit()
+})
