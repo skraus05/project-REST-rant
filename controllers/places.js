f648f6f58dfe0827f33bbdea55e7911301761f54
Samuel Kraus <samuelkraus@Samuels-MBP.lan>
2023-12-07 18:31:38 -0500

    adding edit form, finally

--- controllers/places.js
+++ controllers/places.js
@@ -45,7 +45,14 @@ router.put('/:id', (req, res) => {
 })
 
 router.delete('/:id', (req, res) => {
-    res.send('DELETE /places/:id stub')
+    db.Place.findByIdAndDelete(req.params.id)
+    .then (() => {
+        res.redirect('/places')
+    })
+    .catch(err => {
+        console.log('err', err)
+        res.render('error404')
+    })
 })
 
 router.post('/:id/comment', (req, res) => {
@@ -75,7 +82,13 @@ router.post('/:id/comment', (req, res) => {
 })
 
 router.get('/:id/edit', (req, res) => {
-    res.send('GET edit form stub')
+    db.Place.findById(req.params.id)
+    .then((place) => {
+        res.render('places/edit', { place })
+    })
+    .catch((err) => {
+        res.render('error404')
+    })
 })
 
 router.post('/:id/rant', (req, res) => {
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -24,10 +24,10 @@ function show(data) {
     return (
         <Def>
             <main>
-                <a href={`/places/${data.id}/edit`} className='btn btn-warning'>
+                <a href={`/places/${data.place.id}/edit`} className='btn btn-warning'>
                     Edit
                 </a>
-                <form method='POST' action={`/places/${data.id}?_method=DELETE`}>
+                <form method='POST' action={`/places/${data.place.id}?_method=DELETE`}>
                     <button type='submit' className='btn btn-danger'>
                         Delete
                     </button>
