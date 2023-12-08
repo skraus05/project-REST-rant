4e2467e7e5488458a13fb485a43046bc57ecb758
Samuel Kraus <samuelkraus@Samuels-MBP.lan>
2023-12-08 13:21:32 -0500

    adding rating numbers

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -7,7 +7,21 @@ function show(data) {
             No comments yet!
         </h3>
     )
+    let rating = (
+        <h3 className="inactive">
+            Not yet rated
+        </h3>
+    )
     if (data.place.comments.length) {
+        let sumRatings = data.place.comments.reduce((tot, c) => {
+            return tot + c.stars
+        }, 0)
+        let averageRating = sumRatings / data.place.comments.length
+        rating = (
+            <h3>
+                {Math.round(averageRating)} stars
+            </h3>
+        )
         comments = data.place.comments.map(c => {
             return (
                 <div className="border">
@@ -33,7 +47,9 @@ function show(data) {
                     </button>
                 </form>
                 <h1>{data.place.name}</h1>
-                <section>Rating</section>
+                <h2>Rating</h2>
+                { rating }
+                <br/>
                 <h5>Not rated</h5>
                 <img src={data.place.pic} alt='main restaurant pic'/>
                 <h2>Located in {data.place.city}, {data.place.state}</h2>
